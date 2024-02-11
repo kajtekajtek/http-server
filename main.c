ac78ad3e9fb918fa59d9dfdf2a91754520e600c7
kajtek <kajtek0123@gmail.com>
2024-02-11 22:41:30 +0100

    basic toy server

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "server.h"
+
+int main(int argc, char* argv[])
+{
+	int sock_listen; // socket to listen on
+
+	bind_listener(&sock_listen);
+
+	listen_on(&sock_listen);
+
+	main_loop(&sock_listen);
+
+	return 0;
+}
